d6639a86473e8acf2c730e464dec08527f5a9a6a
wuhaiwei <wuhaiwei0809@163.com>
2016-08-22 15:52:19 +0800

    ..

---
+++ PayPasswordView/HWPayPasswordView.h
@@ -0,0 +1,18 @@
+//
+//  HWPayPasswordView.h
+//  PayPasswordView
+//
+//  Created by  wuhiwi on 16/8/22.
+//  Copyright © 2016年 wanglibank. All rights reserved.
+//
+/**
+ *  第二种方式创建 密码输入框View
+ */
+#import <UIKit/UIKit.h>
+
+@interface HWPayPasswordView : UIView
+
+@property (nonatomic, assign) UInt8 elementCount;
+@property (nonatomic, strong) UIColor *lineColor;
+
+@end
---
+++ PayPasswordView/HWPayPasswordView.m
@@ -0,0 +1,28 @@
+//
+//  HWPayPasswordView.m
+//  PayPasswordView
+//
+//  Created by  wuhiwi on 16/8/22.
+//  Copyright © 2016年 wanglibank. All rights reserved.
+//
+
+#import "HWPayPasswordView.h"
+
+@interface HWPayPasswordView ()
+
+@property (nonatomic, strong) UITextField *passwordTextField;
+@property (nonatomic, strong) NSMutableArray *dataImageViewArray;
+
+@end
+
+
+@implementation HWPayPasswordView
+#pragma mark - init
+
+
+#pragma mark - setter getter
+
+
+
+
+@end
