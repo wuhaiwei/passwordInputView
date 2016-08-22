a167198d3af1d8144f2dbd616a8c9b153c010a8c
wuhaiwei <wuhaiwei0809@163.com>
2016-08-22 11:05:08 +0800

    Initial Commit

---
+++ PayPasswordView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PayPasswordView
+//
+//  Created by  wuhiwi on 16/8/22.
+//  Copyright © 2016年 wanglibank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
